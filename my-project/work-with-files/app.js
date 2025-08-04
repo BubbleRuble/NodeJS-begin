61674c7db64123b672d7d80456cd308e116ea72d
Andriy <jeki4an22@gmail.com>
2025-08-04 18:21:01 +0300

    функції actionи робота з даними(масивом книг)

--- my-project/work-with-files/app.js
+++ my-project/work-with-files/app.js
@@ -1,11 +1,56 @@
-const fs = require("fs/promises");
+// const fs = require("fs/promises");
+const books = require('./books');
+
+const invokeAction = async ({ action, id, title, author }) => {
+  switch (action) {
+    case 'read':
+      const allBooks = await books.getAll();
+      return console.log(allBooks)
+    case 'getById':
+      const oneBook = await books.getById(id);
+      return console.log(oneBook)
+    case 'add':
+      const newBook = await books.add({title, author});
+      return console.log(newBook)
+    case 'updateById':
+        const updateBook = await books.updateById(id, {title, author});
+        return console.log(updateBook)
+    case 'deleteById':
+        const deleteBook = await books.deleteById(id);
+        return console.log(deleteBook)
+    default:
+        return;
+  }
+};
+
+// invokeAction({action: 'read'});
+// invokeAction({ action: 'getById', id: 'YxhM4QDxPeA3SmPHcEZPJ' });
+// invokeAction({
+//   action: 'add',
+//   title: 'The Special One',
+//   author: 'Jose Mourinho',
+// });
+// invokeAction({
+//   action: 'updateById',
+//   id: 'YxhM4QDxPeA3SmPHcEZPJ',
+//   title: 'The Special One',
+//   author: 'Kyryllo Ganin',
+// });
+// invokeAction({action: 'deleteById', id: 'Z0ZAPmU6QMESN6dV205Tc'})
+
+const actionIndex = process.argv.indexOf('--action');
+if (actionIndex !== -1) {
+    const action = process.argv[actionIndex +1];
+    invokeAction({action})
+}
+
 
 // const readFile = async () => {
 //   const text = await fs.readFile('./files/file.txt', 'utf-8');
 //   // const buffer = await fs.readFile('./files/file.txt');
 //   // const text = buffer.toString();
 //   console.log(text)
-// } 
+// }
 
 // readFile()
 
@@ -15,13 +60,12 @@ const fs = require("fs/promises");
 
 // addText()
 
+// const replaceText = async()=> {
+//     const result = await fs.writeFile("./files/file.txt", "лох");
+//     console.log(result);
+// }
 
-const replaceText = async()=> {
-    const result = await fs.writeFile("./files/file.txt", "лох");
-    console.log(result);
-}
-
-replaceText();
+// replaceText();
 
 // fs.readFile("./files/file.txt")
 //     .then(data => console.log(data))
